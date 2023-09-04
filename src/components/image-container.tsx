e89f2bf4baea045cf3455a5da512f04d422823b8
min33sky <min33sky@naver.com>
2023-09-04 23:17:32 +0900

    update: image container

--- src/components/image-container.tsx
+++ src/components/image-container.tsx
@@ -9,8 +9,17 @@ export default function ImageContainer({
   photo,
 }: ImageContainerProps): JSX.Element {
   return (
-    <div key={photo.id} className="h-64 bg-gray-200 rounded-xl">
-      <Image src={photo.src.large} alt={photo.alt} width={250} height={250} />
+    <div
+      key={photo.id}
+      className="group relative h-64 bg-gray-200 rounded-xl overflow-hidden"
+    >
+      <Image
+        src={photo.src.large}
+        alt={photo.alt}
+        fill
+        sizes="(min-width: 1280px) 278px, (min-width: 1040px) calc(12.73vw + 118px), (min-width: 800px) 33.18vw, (min-width: 540px) 50vw, calc(100vw - 16px)"
+        className="object-cover group-hover:opacity-75"
+      />
     </div>
   );
 }
