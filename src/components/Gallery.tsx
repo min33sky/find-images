f27a3fd33fc14f2f81a06b4ea2f58f6951576427
min33sky <min33sky@naver.com>
2023-09-06 01:12:21 +0900

    feat: add placeholder blur effect to images

--- src/components/Gallery.tsx
+++ src/components/Gallery.tsx
@@ -1,6 +1,7 @@
 import fetchImages from '@/lib/fetchImages';
 import { ImagesResults } from '@/schemes/images';
 import ImageContainer from './image-container';
+import addBlurredDataUrls from '@/lib/getBase64';
 
 export default async function Gallery() {
   const url = 'https://api.pexels.com/v1/curated';
@@ -10,9 +11,12 @@ export default async function Gallery() {
   if (!images)
     return <h2 className="m-4 text-2xl font-bold">No Images Found</h2>;
 
+  // 기존의 이미지 데이터에 블러 처리된 이미지 데이터를 추가
+  const photosWithBlur = await addBlurredDataUrls(images);
+
   return (
     <section className="px-2 my-3 grid grid-cols-auto-fit gap-2">
-      {images.photos.map((photo) => (
+      {photosWithBlur.map((photo) => (
         <ImageContainer key={photo.id} photo={photo} />
       ))}
     </section>
--- src/components/image-container.tsx
+++ src/components/image-container.tsx
@@ -5,19 +5,16 @@ interface ImageContainerProps {
   photo: Photo;
 }
 
-export default function ImageContainer({
-  photo,
-}: ImageContainerProps): JSX.Element {
+export default function ImageContainer({ photo }: ImageContainerProps) {
   return (
-    <div
-      key={photo.id}
-      className="group relative h-64 bg-gray-200 rounded-xl overflow-hidden"
-    >
+    <div className="group relative h-64 bg-gray-200 rounded-xl overflow-hidden">
       <Image
         src={photo.src.large}
         alt={photo.alt}
         fill
         sizes="(min-width: 1280px) 278px, (min-width: 1040px) calc(12.73vw + 118px), (min-width: 800px) 33.18vw, (min-width: 540px) 50vw, calc(100vw - 16px)"
+        placeholder="blur"
+        blurDataURL={photo.blurredDataUrl}
         className="object-cover group-hover:opacity-75"
       />
     </div>
